ff7ce394100930afaa5aea077cabb9fed41de8be
Dmytro Kulinich <spo1091@i.ua>
2023-07-21 16:02:48 +0300

    Add HW6_1 Kulinich

---
+++ HW6_1/ListOfProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace HW6_1
+{
+    public class ListOfProduct
+    {
+        public DateTime Date { get; set; }
+
+        public string NameOfShop { get; set; }
+
+        public string List { get; set; }
+    }
+}
